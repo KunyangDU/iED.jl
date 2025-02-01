1260279055e6a4635cc09f53cd4166715340a006
KunyangDU <duky2003@163.com>
2025-02-02 00:37:03 +0800

    reversion ham

--- Heisernberg/main.jl
+++ Heisernberg/main.jl
@@ -77,42 +77,11 @@ function CheckStateRev(s, N, R)
     return R,nothing
 end
 
-function LattRevSymm(N, k, p)
-    ps = []
-    Rs = []
-    ms = []
-    for s in state
-        m = nothing
-        R = CheckStatePeri(s, N ,k)
-        if !isnothing(R)
-            R,m = CheckStateRev(s, N, R)
-        end
-        for σ in [-1,1]
-            σ == -1 && k ∈ [0,N/2] && continue
-            if !isnothing(m)
-                if 1 + σ*p*cos(im*k*m*2pi/N) == 0
-                    R = nothing
-                end
-                if σ == -1 && 1 - σ*p*cos(im*k*m*2pi/N) != 0 
-                    R = nothing
-                end
-            end
-            if !isnothing(R)
-                push!(ps,s)
-                push!(Rs,R)
-                push!(ms,m)
-            end
-        end
-    end
-
-    ps,Rs,ms
-end
 function LattGauge(s, N)
     r = s
     l = 0
     for i in 1:N-1
         t = rotate(s, i, N)
-        showstate(t,N)
         if t < r
             r = t 
             l=i
@@ -137,14 +106,160 @@ function LattRevGauge(s, N)
     return r,l,q
 end
 
-k = 0
+function DiagTerm(state::Vector,N::Int)
+    ds = zeros(length(state))
+    for (is,s) in enumerate(state)
+        for i in 1:N
+            j = mod(i,N) + 1
+            if bit(s,i) == bit(s,j)
+                ds[is] += 1/4
+            else
+                ds[is] += -1/4
+            end
+        end
+    end
+    return ds
+end
+
+function _getSubsize(ind::Int, state::Vector)
+    if ind > 1 && state[ind] == state[ind-1]
+        return nothing 
+    elseif ind < length(state) && state[ind] == state[ind+1]
+        return 2
+    else
+        return 1
+    end
+end
+
+function OffdiagTerm(i,j,l,q)
+    return 0
+end
+
+function LattRevSymm(N, k, p)
+    ps = []
+    Rs = []
+    ms = []
+    σs = []
+    for s in state
+        
+        m = nothing
+        R = CheckStatePeri(s, N ,k)
+        if !isnothing(R)
+            R,m = CheckStateRev(s, N, R)
+        end
+        R0 = R
+        for σ in [1,-1]
+            R = R0
+            σ == -1 && k ∈ [0,N/2] && continue
+            if !isnothing(m)
+                if 1 + σ*p*cos(k*m*2pi/N) == 0
+                    R = nothing
+                end
+                if σ == -1 && 1 - σ*p*cos(k*m*2pi/N) != 0 
+                    R = nothing
+                end
+            end
+            if !isnothing(R)
+                push!(ps,s)
+                push!(Rs,R)
+                push!(ms,m)
+                push!(σs,σ)
+            end
+        end
+    end
+
+    ps,Rs,ms,σs
+end
+
+function Norm(N,k,R,σ,p,m)
+    if k ∈ [0,N/2]
+        g=1
+    else
+        g=2
+    end
+    if isnothing(m)
+        return N^2*g/R 
+    else
+        return N^2*g*(1+σ*p*cos(k*m*2pi/N))/R
+    end    
+end
+
+k = 1
 p = 1
 
-ps,Rs,ms = LattRevSymm(N, k, p)
-LattRevGauge(flip(ps[1],[4,5]),N),ps
+ps,Rs,ms,σs = LattRevSymm(N, k, p)
+@show ps
+ds = DiagTerm(ps,N)
+
+H = zeros(length(ps),length(ps))
+
+for (ia,a) in enumerate(ps)
+    n = _getSubsize(ia,ps)
+    isnothing(n) && continue
+
+    for i in ia:ia+n-1
+        H[i,i] += ds[i]
+    end
+
+    for i in 1:N 
+        j = mod(i,N) + 1
+        bit(a,i) == bit(a,j) && continue
+        t = flip(a,[i,j])
+        b,l,q = LattRevGauge(t, N)
+        ib = findfirst(x -> x == b, ps)
+        @show b,l,q
+        if !isnothing(ib)
+            if ib > 1 && ps[ib] == ps[ib-1]
+                m = 2
+                ib -= 1
+            elseif ib < length(ps) && ps[ib] == ps[ib+1]
+                m = 2
+            else
+                m = 1
+            end
+
+            for i1 in ia:ia+n-1, j1 in ib:ib+m-1
+                term = let 
+                    nm = (1/2) * (σs[i1]*p)^q * /(map(x -> Norm(N,k,Rs[x],σs[x],p,ms[x]),[j1,i1])...)
+                    c = let 
+                        if isnothing(ms[j1])
+                            if σs[i1] == σs[j1]
+                                @show 1
+                                cos(k*l*2pi/N)
+                            else
+                                @show 2
+                                σs[j1]*sin(k*l*2pi/N)
+                            end
+                        else
+                            if σs[i1] == σs[j1]
+                                @show 3
+                                (cos(k*l*2pi/N) + σs[j1]*p*cos(k*(l-ms[j1])*2pi/N)) / (1 + σs[j1]*p*cos(k*ms[j1]*2pi/N))
+                            else
+                                @show 4
+                                σs[j1]*(sin(k*l*2pi/N) + σs[j1]*p*sin(k*(l-ms[j1])*2pi/N)) / (1 + σs[j1]*p*cos(k*ms[j1]*2pi/N))
+                            end
+                        end    
+                    end
+                    @show nm,c
+                    nm*c
+                end
+                @show i1,j1,term
+                H[i1,j1] += term
+            end
+        end
+    end
+end
+
+H
+#eigvals(H)[1]
 
 #= 
 Hamiltonian for given state ps
+=#
+
+#= 
+分块化有问题
+没有成功按sigma子空间划分
 =#
 
 
