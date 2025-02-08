26b28178c742ed335d7272c616d3ab72bfc24c38
KunyangDU <duky2003@163.com>
2025-02-08 12:38:58 +0800

    ed

---
+++ FermiHubbard/main.jl
@@ -0,0 +1,109 @@
+using LinearAlgebra,CairoMakie
+
+function apply_annihilate(j, s)
+    if (s & (1 << j)) == 0
+        return (0, 0)
+    end
+    s_new = s ⊻ (1 << j)
+    mask = (1 << j) - 1
+    num_before = count_ones(s & mask)
+    sign = (-1)^num_before
+    return (s_new, sign)
+end
+
+function apply_create(j, s)
+    if (s & (1 << j)) != 0
+        return (0, 0)
+    end
+    s_new = s | (1 << j)
+    mask = (1 << j) - 1
+    num_before = count_ones(s & mask)
+    sign = (-1)^num_before
+    return (s_new, sign)
+end
+
+function build_hamiltonian(N; t,μ)
+    dim = 2^N
+    H = zeros(Float64, dim, dim)
+    for s in 0:dim-1 # Open boundary condition
+        for i in 0:N-2 
+            # Term: -t * c_i† c_{i+1}
+            (s1, sign1) = apply_annihilate(i+1, s)
+            (s2, sign2) = apply_create(i, s1)
+            H[s2+1, s+1] += -t * sign1 * sign2
+
+            # Hermitian conjugate term: -t * c_{i+1}† c_i
+            (s1_herm, sign1_herm) = apply_annihilate(i, s)
+            (s2_herm, sign2_herm) = apply_create(i+1, s1_herm)
+            H[s2_herm+1, s+1] += -t * sign1_herm * sign2_herm
+        end
+
+        H[s+1,s+1] += -μ*count_ones(s)
+    end
+    return H
+end
+
+function ϵ(k;t,μ)
+    return -2*t*cos(k)-μ
+end
+
+function getGStheo(N;t,μ)
+    k = pi*(1:N) / (N+1)
+    ϵs = filter!(x -> x<0, ϵ.(k;t=t,μ = μ))
+    E = sum(ϵs)
+    N = length(ϵs)
+    return E,N
+end
+
+function getNop(N)
+    Nop = zeros(2^N,2^N)
+    for s in 0:2^N-1
+        Nop[s+1,s+1] = count_ones(s)
+    end
+    return Nop
+end
+
+# Example usage
+N = 10
+t = 1.0
+
+lsμ = range(-2,2,50)
+
+Nop = getNop(N)
+
+Es = []
+Est = []
+Nst = []
+Ns = []
+for μ in lsμ
+    params = (
+        t = t,
+        μ = μ
+    )
+    H = build_hamiltonian(N;params...)
+    F = eigen(H)
+    eigenvalues = F.values
+    eigenvectors = F.vectors
+    n = eigenvectors[:,1]'*Nop*eigenvectors[:,1]
+    push!(Es,eigenvalues[1])
+    push!(Ns,n)
+    Et,Nt = getGStheo(N;params...)
+    push!(Est,Et)
+    push!(Nst,Nt)
+end 
+
+fig = Figure()
+ax = Axis(fig[1,1];
+title = "Spinless free fermion",
+width = 400,
+height = 200,
+xlabel = L"\mu",
+ylabel = L"n")
+
+lines!(lsμ,Nst / N;color = :red,label = "Theory")
+scatter!(lsμ,Ns / N;label = "DS-R1 code")
+axislegend(ax,position = :lt)
+resize_to_layout!(fig)
+display(fig)
+
+save("FermiHubbard/figures/DS_spinless_free.png",fig)
--- Heisenberg/test.jl
+++ Heisenberg/test.jl
@@ -1,8 +1,43 @@
+using CairoMakie,LaTeXStrings
 
 include("utils.jl")
 
-state = U1Symm(8,1)
-
-for (is,s) in enumerate(state) 
-    
+function main_U1pLatt(N = 4,m=0,lsk=0:N-1;Jxy=1,Jzz=1)
+    state = U1Symm(N,m)
+    Hs = Vector(undef,length(lsk))
+    Hs .= nothing
+    stateinfos = Vector(undef,length(lsk))
+    stateinfos .= nothing
+    ks = Vector(undef,length(lsk))
+    ks .= nothing
+    for (ik,k) in enumerate(lsk)
+        pstate,Rs = LattSymm(N, k, state)
+        isempty(pstate) && continue
+        L = length(pstate)
+        H = zeros(L,L) * 1im
+        for (ia,a) in enumerate(pstate)
+            for i in 1:N
+                j = mod(i,N) + 1
+                if bit(a,i) == bit(a,j)
+                    H[ia,ia] += 1/4 * Jzz
+                else
+                    H[ia,ia] += - 1/4 * Jzz
+                    b = flip(a,[i,j])
+                    b,l = LattGauge(b, N)
+                    ib = findfirst(x -> x == b, pstate)
+                    if !isnothing(ib)
+                        H[ia,ib] += (Jxy) * 1/2 * sqrt(Rs[ia]/Rs[ib]) * exp(1im*k*l * 2pi / N)
+                    end
+                end
+            end
+        end
+        Hs[ik] = hermitianize(H)
+        stateinfos[ik] = Dict("pstate" => pstate,"Rs" => Rs)
+        ks[ik] = k
+    end
+    return Hs,stateinfos,ks
 end
+
+Hs,~,~ = main_U1pLatt(18,0,[0,])
+#@time F = eigen(Hs[1])
+size(Hs[1])
